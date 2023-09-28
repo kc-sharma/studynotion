014c712420721fd347aea3b549f8686905496126
Ayush Sharma <ayushsharma3310@gmail.com>
2023-09-28 16:24:03 +0530

    correcting url 4

--- server/index.js
+++ server/index.js
@@ -23,7 +23,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
 	cors({
-		origin:"https://studynotion-sigma.vercel.app/",
+		origin:"https://studynotion-sigma.vercel.app",
 		credentials:true,
 	})
 )
